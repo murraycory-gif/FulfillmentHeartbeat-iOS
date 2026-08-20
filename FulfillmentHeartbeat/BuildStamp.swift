bc8d3c69fa1bc339862b6ea3402af44dfb4a332a
murraycory-gif <murraycory-gif@users.noreply.github.com>
2026-08-20 01:23:31 +0000

    Restore the Choose file importer and xlsx reader that worked all day

--- FulfillmentHeartbeat/BuildStamp.swift
+++ FulfillmentHeartbeat/BuildStamp.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 enum BuildStamp {
-    static let id = "HB-0819.41"
+    static let id = "HB-0819.42"
 
     static var version: String {
         Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "1.0"
